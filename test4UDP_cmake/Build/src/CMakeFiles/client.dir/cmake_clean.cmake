6bb6462f11674de7bb0b61163b28049142d9e834
spring <spring@spring-Lenovo.(none)>
2012-07-07 13:03:31 +0800

    add

---
+++ test4UDP_cmake/Build/src/CMakeFiles/client.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/client.dir/client_main.c.o"
+  "../bin/client.pdb"
+  "../bin/client"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang C)
+  INCLUDE(CMakeFiles/client.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
