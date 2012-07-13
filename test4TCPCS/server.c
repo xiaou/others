d10dffca0043637c1bff5bb602692934ef90c546
spring <spring@spring-Lenovo.(none)>
2012-07-13 09:58:27 +0800

    add

--- test4TCPCS/server.c
+++ test4TCPCS/server.c
@@ -1,5 +1,7 @@
 #include "unp.h"
 
+static int g_bindSockfd = 0;
+
 void
 sig_chld_(int signo)
 {printf(".\n");
@@ -9,6 +11,12 @@ sig_chld_(int signo)
 	while ( (pid = waitpid(-1, &stat, WNOHANG)) > 0)
 		printf("child %d terminated!\n", pid);
 	return;
+}
+
+void sig_ctrl_(int signo)
+{printf("sig_strl.\n");
+	close(g_bindSockfd);
+	exit(0);
 }
 
 void
@@ -39,19 +47,23 @@ int main()
 
 	memset(&servaddr, 0x00, sizeof(servaddr));
 	servaddr.sin_family = AF_INET;
-	servaddr.sin_port = htons(SERV_PORT);
+	servaddr.sin_port = htons(SERV_PORT+4);
 	servaddr.sin_addr.s_addr = htonl(INADDR_ANY);
 	
 	if(bind(listenfd, (struct sockaddr*)&servaddr, sizeof servaddr) == -1)
 	{
 		printf("bind faile. errno:%d\n", errno);
 		return -1;
-	} 
+	}
+	
+	g_bindSockfd = listenfd;
 	
 	if(listen(listenfd, 10) == -1)
 		return -1;
 	
 	Signal(SIGCHLD, sig_chld_);
+	Signal(SIGTSTP, sig_ctrl_);
+	Signal(SIGINT, sig_ctrl_);
 
 	while(1)
 	{
