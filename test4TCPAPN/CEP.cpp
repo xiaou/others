9442975a752cd3b8d90b1e54a250651b3f0c8799
xiaou <xiaou@mini.local>
2012-08-12 14:26:14 +0800

    add CEP

--- test4TCPAPN/CEP.cpp
+++ test4TCPAPN/CEP.cpp
@@ -3,3 +3,47 @@
 
 
 
+int CEP::runloop_epoll_wait()
+{
+	if(m_epfd == -1)
+		return -1;
+	
+	struct epoll_event * ep_evs_buf;
+	size_t n_ep_evs_buf = 128;
+	if((ep_evs_buf = (struct epoll_event *)malloc(n_ep_evs_buf * sizeof(struct epoll_event))) == NULL)
+	{
+		std::cerr<<"malloc faild.error["<<errno<<"]:"<<strerror(errno)<<std::endl;
+		return -1;
+	}
+	while(1)
+	{
+		checkTime();
+		
+		size_t currEventsNum = m_events.size();
+		if(currEventsNum == 0)
+			return 0;
+		if(currEventsNum > n_ep_evs_buf)
+		{
+			n_ep_evs_buf = currEventsNum + 128;
+			free(ep_evs_buf);
+			if((ep_evs_buf = (struct epoll_event *)malloc(n_ep_evs_buf * sizeof(struct epoll_event))) == NULL)
+			{
+				std::cerr<<"malloc() faild.error["<<errno<<"]:"<<strerror(errno)<<std::endl;
+				return -1;
+			}
+		}
+	
+		int nReady = epoll_wait(m_epfd, ep_evs_buf, currEventsNum, 2000);
+		if(nReady < 0)
+		{
+			std::cerr<<"epoll_wait() faild.error["<<errno<<"]:"<<strerror(errno)<<std::endl;
+			return -1;
+		}
+		for(int i = 0; i != nReady; i++)
+		{
+			handleEvent(&ep_evs_buf[i]);
+		}
+		
+	}
+	return 0;
+}
--- test4TCPAPN/CEP.h
+++ test4TCPAPN/CEP.h
@@ -1,5 +1,8 @@
-///need kernel2.6.9
-
+/*
+ * 
+ * note: 1. need kernel2.6.9
+ *       
+ */
 
 #ifndef _CEP_H_
 #define _CEP_H_
@@ -17,29 +20,49 @@
 #include <errno.h>
 #include <string.h>
 #include <iostream>
-
+#include <vector>
 
 #define	 MAXDATABUFSIZE	   1024 /**< 一个事件的数据buffer最大1kb,太大了悲剧，会分包(分包我不管).*/
 
-struct CEP_Event
+enum CEPEvType
 {
-	
-
+	CEPEvType_Connect;
+	CEPEvType_Listen;
+	CEPEvType_Send;
+	CEPEvType_Recv;
 };
 
-/** @brief epoll.ET模式.fd必须非阻塞. */
+struct CEP_Event
+{
+	int fd;  
+	CEPEvType type;
+    void (*call_back)(int fd, int events, void *arg);  
+    int events;  
+    //void *arg;  
+    //int status; // 1: in epoll wait list, 0 not in  
+    char buf[MAXDATABUFSIZE];   
+    int len;  
+    long last_active; // last active time
+};
+
+/** @brief epoll.ET模式.fd非阻塞. */
 class CEP
 {
-	public:
-	CEP(){ if((m_epfd = epoll_create(1)) == -1) std::cerr<<"faild in CEP()."<<std::endl; }
+public:
+	CEP(){ if((m_epfd = epoll_create(1)) == -1) std::cerr<<"faild in CEP().error["<<errno<<"]:"<<strerror(errno)<<std::endl; }
 	virtual ~CEP(){ close(m_epfd); }
 	virtual int addEvent(CEP_Event * cep_ev);
 	virtual int modEvent(CEP_Event * cep_ev);
 	virtual int delEvent(CEP_Event * cep_ev);
-	virtual int waitEvents();
+	size_t currEventsNum(){ return m_events.size(); }
+	virtual int runloop_epoll_wait();/**< retval -1 error. */
 
-	protected:
+protected:
+	virtual void checkTime();		
+	virtual void handleEvent(struct epoll_event * ep_ev);
+		
 	int m_epfd;
+	std::vector<CEP_Event> m_events;
 };
 
 #endif // #ifndef _CEP_H_
