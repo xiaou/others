163aded10fb2adeb7aeae7d48cd7f29e6fed48c7
spring <spring@spring-Lenovo.(none)>
2012-08-15 11:22:31 +0800

    a

---
+++ test4Log/build/CMakeFiles/test.dir/DependInfo.cmake
@@ -0,0 +1,13 @@
+# The set of languages for which implicit dependencies are needed:
+SET(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+SET(CMAKE_DEPENDS_CHECK_CXX
+  "/home/spring/myGits/others/test4Log/main.cpp" "/home/spring/myGits/others/test4Log/build/CMakeFiles/test.dir/main.cpp.o"
+  )
+SET(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Targets to which this target links.
+SET(CMAKE_TARGET_LINKED_INFO_FILES
+  )
