808dfacfcb7ba6e8e59489ace43c15e5709049c3
xiaou <xiaou@xiaou-K50IN.(none)>
2012-10-06 02:31:30 +0800

    a

---
+++ test4Boost/callback_task.hpp
@@ -0,0 +1,29 @@
+#ifndef __callback_task_h__
+#define __callback_task_h__
+
+#include <boost/function.hpp>
+
+namespace boost { namespace threadpool
+{
+	
+template<class RetType>
+class callback_task
+{
+	typedef boost::function<void (RetType)> CALLBACK;
+	typedef boost::function<RetType ()> FUNCTION;
+
+private:
+	CALLBACK m_Callback;
+	FUNCTION m_Function;
+
+public:
+	callback_task(FUNCTION f, CALLBACK c):m_Callback(c), m_Function(f){}
+
+	void operator()(){ m_Callback(m_Function()); }
+};
+
+
+}} // namespace boost::threadpoll
+
+#endif // __callback_task_h__
+
--- test4Boost/main.cpp
+++ test4Boost/main.cpp
@@ -1,51 +1,121 @@
-
 #include <iostream>
+#include <sstream>
 #include <boost/threadpool.hpp>
-//#include <unistd.h>
+#include "callback_task.hpp"
 
 using namespace std;
 using namespace boost::threadpool;
 
-// Some example tasks
-void first_task()
-{
-    cout << "first task is running\n" ;
-	sleep(.2);
-}
 
-void second_task()
+void task_normal()
 {
-    cout << "second task is running\n" ;
-	sleep(.2);
+    cout << "task_normal()\n";
 }
 
 void task_with_parameter(int value, string str)
 {
     cout << "task_with_parameter(" << value << ", " << str << ")\n";
-	sleep(.2);
 }
 
-int task_return_14()
+bool task_loop()
+{
+	static int i = 0;
+	cout << "task_loop:" << i <<"\n";
+	return ++i != 5;
+}
+
+int task_return14()
 { 
+	sleep(1);
 	return 14; 
+}
+
+void callback(int ret)
+{
+	cout<< "callback: task_return14() return " << ret << "\n";
+}
+
+void task_test4ThreadPrivateData()
+{
+	cout << "task_test4ThreadPrivateData().id:";
+
+	static map<boost::thread::id, string> s_ThreadPrivateData;
+
+	boost::thread::id tid = boost::this_thread::get_id();
+	cout << tid << "\n";
+
+	map<boost::thread::id, string>::iterator it;
+	if((it = s_ThreadPrivateData.find(tid)) == s_ThreadPrivateData.end())
+	{
+		it = s_ThreadPrivateData.insert(make_pair(tid, "hello")).first;
+	}
+
+	cout << tid << " has private data:" << it->second << "\n";
+}
+
+void help2SeePoolStatus(pool & tp)
+{
+	ostringstream os;
+	os << "begin>\n";
+
+	os << "how many threads in the pool:" << tp.size() << "\n";
+	os << "how many tasks are currently executed:" << tp.active() << "\n";
+	os << "how many tasks are ready and waiting for execution:" << tp.pending() << "\n";
+
+	os << "<end.";
+	cout<< "\033[1;45;33m"<< os.str() << "\033[0m" << "\n";
+}
+
+void help2AddAllTask(pool & tp)
+{
+	tp.schedule( callback_task<int>(&task_return14, callback) );
+    tp.schedule(&task_normal);
+    tp.schedule(boost::bind(task_with_parameter, 4, "number"));
+	tp.schedule( looped_task_func(&task_loop, 0.5*1000) );	
+	tp.schedule(&task_test4ThreadPrivateData);
+}
+
+void testCase0()
+{
+	cout<< "testCase0()\n" << endl;
+    
+	// Create fifo thread pool container with n threads.
+	pool tp(0);// 0 threads in pool
+	
+	help2AddAllTask(tp);
+
+	help2SeePoolStatus(tp);
+	
+	//Wait until all task are finished.
+	tp.wait();
+}
+
+void testCase1()
+{
+	cout<< "testCase1()\n" << endl;
+	pool tp(1);// only one thread in pool.
+	help2AddAllTask(tp);
+	help2SeePoolStatus(tp);
+	tp.size_controller().resize(5);
+	help2SeePoolStatus(tp);
+	tp.wait();
+	help2SeePoolStatus(tp);
+}
+
+void testCase2()
+{
+	cout<< "testCase2()\n" << endl;
+	pool tp(10);
+	help2AddAllTask(tp);
+	for(int i = 0; i != 4; i++, help2SeePoolStatus(tp), sleep(.5));
+	tp.wait();
 }
 
 int main(int argc,char *argv[])
 {
-    // Create fifo thread pool container with two threads.
-    pool tp(2);
+	testCase1();
 
-    // Add some tasks to the pool.
-    tp.schedule(&first_task);
-    tp.schedule(&second_task);
-    tp.schedule(boost::bind(task_with_parameter, 4, "number"));
-	future<int> res = schedule(tp, &task_return_14);
-
-    // Wait until all tasks are finished.
-    tp.wait();
-
-	cout<< "get res value for task_return_14():" << res.get() <<endl;
-
-    // Now all tasks are finished!    
     return(0);
 }
+
+
