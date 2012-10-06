b7f3cf11202df5ee92071e14363124d578845c6d
xiaou <xiaou@xiaou-K50IN.(none)>
2012-10-06 22:02:34 +0800

    a

--- test4Log/main.cpp
+++ test4Log/main.cpp
@@ -1,40 +1,33 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <string.h>
-#include <assert.h>
-#include <sys/types.h>
-#include <sys/wait.h>
-#include <signal.h>
-#include <iostream>
-#include <errno.h>
-#include <dirent.h>
-#include <sys/stat.h>
-#include "LOG.h"
-#include "log4cplus/logger.h"
-#include "log4cplus/consoleappender.h"
-#include "log4cplus/fileappender.h"
-#include "log4cplus/layout.h"
-#include "log4cplus/loggingmacros.h"
-#include "LOG.h"
 
+#include <iostream>
 
 using namespace std;
-using namespace log4cplus;
 
 
 int main()
 {
-	LOG_GLOBAL_INIT(0, "../conf", 1, 0, 0);
+string s;
 
-do
-{
-	LOG_DEBUG("2");LOG_DEBUG("2");LOG_DEBUG("2");
-LOG_ERROR("0");
+cout << s.length() << endl;
 
-}while(usleep(1000*1000) == 0);
-	
-	cout<<"return 0;"<<endl;
-	return 0;
+s.resize(4);
+
+cout << s.length() << endl;
+
+cout << s.capacity() << endl;
+
+s += "xx00";
+s.resize(10);
+s += "yyjj";
+cout << s << endl;
+
+s = "";
+s.resize(1);
+cout << s.capacity() << endl;
+
+cout << s.max_size() << endl;
+
+return 0;
+
 }
 
