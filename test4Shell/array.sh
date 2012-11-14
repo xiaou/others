430bf332e4c6d2b76e1fdea5e63f6630b02ab075
spring <spring@spring-Lenovo.(none)>
2012-11-14 14:14:41 +0800

    a

--- test4Shell/array.sh
+++ test4Shell/array.sh
@@ -1,16 +1,28 @@
 #! /bin/bash
 
+echo $0
+
 ARR=(1 2 3 4 5)
-len=
+LEN=${#ARR[*]}  # '#'就是用来取长度的.
 
 echo ${ARR[*]}
+echo LEN=$LEN
 
+echo add:
+ARR[5]=6
+echo ${ARR[*]}
 
+echo "change index 3:"
+ARR=(${ARR[*]/$((3+1))/44})
+echo ${ARR[*]}
 
+echo "from index 2, len = 3:"
+ARR2=(${ARR[*]:2:3})
+echo ${ARR2[*]}
 
-
-
-
-
-
-
+echo erase index 0:
+unset ARR[0]
+echo ${ARR[*]}
+echo erase all:
+unset ARR
+echo ${ARR[*]}
--- test4Shell/test.sh
+++ test4Shell/test.sh
@@ -1,5 +1,12 @@
 #! /bin/sh
 
+v1=val1
+r1=v1
+eval v2='$'$r1
+echo $v1
+echo $v2
+
+exit
 
 x=4
 
